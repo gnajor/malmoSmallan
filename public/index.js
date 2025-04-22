8c2a24b0497367d2ed4d47b8ba1d4fab6fc19672
Ella Ahlstedt <ellaahl02@gmail.com>
2025-04-22 14:06:59 +0200

    implemented messages page

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 import { renderBankPage } from "./pages/bankPage/bankPage.js";
 import { renderHomePage } from "./pages/homePage/homePage.js";
+import { renderMessagesPage } from "./pages/messagesPage/messages.js";
 import { renderNotesPage } from "./pages/notesPage/notesPage.js";
 
 const state = {
@@ -20,35 +21,35 @@ const gameData = {
             icon: "maps-app.jpg",
             name: "Kartor",
             func: () => {
-                
+
             }
         },
         {
             icon: "notes-app.jpg",
             name: "Anteckningar",
             func: () => {
-                
+
             }
         },
         {
             icon: "phone-app.png",
             name: "Telefon",
             func: () => {
-                
+
             }
         },
         {
             icon: "bank-app.jpg",
             name: "DegBanken",
             func: () => {
-                
+
             }
         },
         {
             icon: "news-app.jpg",
             name: "Malmöbladet",
             func: () => {
-                
+
             }
         }
     ],
@@ -65,7 +66,7 @@ const gameData = {
             name: "Star bar",
             date: "2025-04-06",
             sum: "-79,99"
-        },   
+        },
         {
             icon: "wine.svg",
             name: "Mascot",
@@ -77,37 +78,37 @@ const gameData = {
             name: "Indian Express 2",
             date: "2025-04-06",
             sum: "-243,00"
-        },    
+        },
         {
             icon: "money.svg",
             name: "Bankomat",
             date: "2025-04-06",
             sum: "+10000,00"
-        }, 
+        },
         {
             icon: "utensils.svg",
             name: "V.E.D",
             date: "2025-04-06",
             sum: "-2000,00"
-        },   
+        },
         {
             icon: "utensils.svg",
             name: "Sibylla",
             date: "2025-04-06",
             sum: "-693,00"
-        },  
+        },
         {
             icon: "utensils.svg",
             name: "Gallia",
             date: "2025-04-06",
             sum: "-2999,00"
-        },  
+        },
         {
             icon: "wine.svg",
             name: "Square Side",
             date: "2025-04-06",
             sum: "-180,00"
-        },  
+        },
         {
             icon: "wine.svg",
             name: "Azalee",
@@ -125,11 +126,59 @@ const gameData = {
             name: "Vegan bar",
             date: "2025-04-06",
             sum: "-140,00"
-        }  
+        }
+    ],
+
+    friendMessages: [
+        { sender: "Spelaren", text: "Vi kör svamppasta, hittade riktigt fina svampar på torget", normal: true },
+        { sender: "Kompis", text: "Det blir dunder", normal: true },
+        { sender: "Spelaren", text: "Har några öl i kylen, köper du en flaska vin?", normal: true },
+        { sender: "Kompis", text: "Jag fixar!", normal: true },
+        { sender: "Spelaren", text: "Kom vid 18, ta på dig något fint", normal: true },
+        { sender: "Spelaren", text: "Kom till star bar", time: "Igår 21:15" },
+        { sender: "Kompis", text: "Jag är hemma nu" },
+        { sender: "Spelaren", text: "Kom till mascot", time: "Igår 21:30" },
+        { sender: "Spelaren", text: "Kom till Ved", time: "Igår 23:00" },
+        { sender: "Kompis", text: "Ska sova nu" },
+        { sender: "Spelaren", text: "Kom till Gallia", time: "Idag 00:00" },
+        { sender: "Kompis", text: "Vem är du med?" },
+        { sender: "Spelaren", text: "Kom till Squareside", time: "Idag 00:30" },
+        { sender: "Spelaren", text: "Kom till azalee", time: "Idag 01:00" },
+        { sender: "Spelaren", text: "Jag bjuder" },
+        { sender: "Spelaren", text: "Det står en limo utanför dig nu", time: "Idag 01:30" },
+        { sender: "Kompis", text: "Vad händerrrr???????" },
+        { sender: "Spelaren", text: "Svart eller röd?" },
+        { sender: "Kompis", text: "Fattar ingenting, vad håller du på med??!!" },
+        { sender: "Spelaren", text: "Hallå?" },
+        { sender: "Kompis", text: "vad händer" },
+        { sender: "Kompis", text: "Jag är på möllan vart är du?", time: "Idag 02:55" },
+        { sender: "Kompis", text: "Hallå. Vart är du på väg?", time: "Idag 03:01" },
+        { sender: "Spelaren", text: "Måste fixa en sak" },
+        { sender: "Spelaren", text: "Kan du swosha mig?" },
+        { sender: "Kompis", text: "Vad fan hände med dig igår?", time: "Idag 9:35" },
+        { sender: "Spelaren", text: "Minns bara att jag var med er, sen minns jag inget.", canSend: true },
+        { sender: "Kompis", text: "Sist jag såg dig sprang du mot Pildammstornet med en svart väska. Du skrek att du behövde gömma pengar eller nåt. Du hade gps framme medan du sprang. (Du kan kolla där.)" },
+    ],
+    friendMessagesNormal: [
+        { sender: "Spelaren", text: "Vi kör svamppasta, hittade riktigt fina svampar på torget" },
+        { sender: "Kompis", text: "Det blir dunder" },
+        { sender: "Spelaren", text: "Har några öl i kylen, köper du en flaska vin?" },
+        { sender: "Kompis", text: "Jag fixar!" },
+        { sender: "Spelaren", text: "Kom vid 18, ta på dig något fint" },
+        { sender: "Kompis", text: "Vad var det för svampar igår, har mått riktigt konstigt." },
+        { sender: "Spelaren", text: "Så sjukt… vaknade precis upp på Möllan,tror jag hallucinerade.", canSend: true },
+    ],
+    dealer: [
+        { sender: "Knarklangare", text: "Du måste hålla pengarna till imorgon, polisen är efter mig.", time: "" },
+        { sender: "Knarklangare", text: "Jag tror att jag kommit undan. Möt mig på triangeln imorgon." },
+        { sender: "Knarklangare", text: "Ge mig mina pengar. Mitt nummer är Möllevångstorgets staty, bussen och Indian express." },
+
     ]
 }
 
-renderBankPage(document.querySelector("#wrapper"), gameData.transactions);
+// renderMessagesPage(document.querySelector("#wrapper"), gameData.friendMessages, "Alex");
+
+// renderBankPage(document.querySelector("#wrapper"), gameData.transactions);
 
 /* renderHomePage(document.querySelector("#wrapper"), gameData.apps) */
 
