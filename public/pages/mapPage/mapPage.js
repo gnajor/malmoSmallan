7db9554554936fdda89cdda0d0369323d6d6bd18
gnajor <lejonet04@gmail.com>
2025-05-03 19:42:26 +0200

    testing

--- public/pages/mapPage/mapPage.js
+++ public/pages/mapPage/mapPage.js
@@ -3,23 +3,18 @@ import * as L from 'https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js';
 export function renderMapPage(parent){
     parent.innerHTML = `<div id="map"></div>`
 
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    setTimeout(() => {
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; OpenStreetMap & CartoDB',
-        subdomains: 'abcd',
-        maxZoom: 19
-      }).addTo(map);
-      
-      // Add a marker
-      L.marker([51.505, -0.09]).addTo(map)
+        const map = L.map('map').setView([51.505, -0.09], 13);
+        
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors',
+            maxZoom: 19
+          }).addTo(map);
+        
+        // Add a marker
+        L.marker([51.505, -0.09]).addTo(map)
         .bindPopup('Waypoint')
         .openPopup();
-   /*  parent.innerHTML = `<div id="map"></div>`; */
-
-/*     var map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map); */
+    }, 50);
 }
