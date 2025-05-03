d692968af013304453ebb6924788fbe68d18c7fa
gnajor <lejonet04@gmail.com>
2025-05-03 19:34:28 +0200

    trying to get map working

--- public/pages/mapPage/mapPage.js
+++ public/pages/mapPage/mapPage.js
@@ -1,4 +1,20 @@
+import * as L from 'https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js';
+
 export function renderMapPage(parent){
+    parent.innerHTML = `<div id="map"></div>`
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; OpenStreetMap & CartoDB',
+        subdomains: 'abcd',
+        maxZoom: 19
+      }).addTo(map);
+      
+      // Add a marker
+      L.marker([51.505, -0.09]).addTo(map)
+        .bindPopup('Waypoint')
+        .openPopup();
    /*  parent.innerHTML = `<div id="map"></div>`; */
 
 /*     var map = L.map('map').setView([51.505, -0.09], 13);
