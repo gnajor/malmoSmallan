eb8bc69c8c1bdb2cc623991f10b061c59762e98e
Ella Ahlstedt <ellaahl02@gmail.com>
2025-05-05 14:41:32 +0200

    police video with sound

---
+++ public/pages/policeLights/policeLights.js
@@ -0,0 +1,9 @@
+export function renderPoliceLights(parent) {
+    parent.innerHTML = `
+        <video autoplay loop class="videoElement">
+            <source src="../../media/video-files/policeLights.mp4">
+        </video>
+    `;
+    const audio = new Audio("../../media/audio-files/policeSound.mp3");
+    audio.play();
+}
